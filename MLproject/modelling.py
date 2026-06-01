e0657411cc768368e2a39f5386ac66bd4eac6396
yudis2 <yudisdwi1@gmail.com>
2026-06-01 23:04:28 +0700

    upload workflow

---
+++ MLproject/modelling.py
@@ -0,0 +1,38 @@
+import mlflow
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import random
+import numpy as np
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("Dropout Students")
+
+data = pd.read_csv("clean_data.csv")
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data.drop("Status", axis=1),
+    data["Status"],
+    random_state=42,
+    test_size=0.2
+)
+input_example = X_train[0:5]
+
+with mlflow.start_run():
+    # Log parameters
+    n_estimators = 505
+    max_depth = 37
+    mlflow.autolog()
+    # Train model
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+    )
+    model.fit(X_train, y_train)
+    # Log metrics
+    accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("accuracy", accuracy)
